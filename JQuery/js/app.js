7e631e18320466745ac1220172755b11906e0a33
Jonathan Kechter <joni.kechter@gmail.com>
2025-02-27 15:25:47 +0100

    Implements Basic App with Login and Registration Basic  Auth Check

---
+++ JQuery/js/app.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    console.log("jQuery Shop Loaded");
+});
---
+++ JQuery/js/auth.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    checkAuth();
+
+    $("#login-form").on("submit", function (event) {
+        event.preventDefault();
+        loginUser();
+    });
+
+    $("#signup-form").on("submit", function (event) {
+        event.preventDefault();
+        signupUser();
+    });
+});
+
+function checkAuth() {
+    let authToken = localStorage.getItem("authToken");
+    if (authToken) {
+        let currentPage = window.location.pathname.split("/").pop();
+        
+        if (currentPage === "login.html" || currentPage === "signup.html") {
+            window.location.href = "products.html";
+        }
+    } else {
+        let restrictedPages = ["products.html", "cart.html"];
+        let currentPage = window.location.pathname.split("/").pop();
+
+        if (restrictedPages.includes(currentPage)) {
+            window.location.href = "login.html";
+        }
+    }
+}
+
+function loginUser() {
+    let email = $("#email").val();
+    let password = $("#password").val();
+
+    const requestData = { email, password };
+
+    $.ajax({
+        url: "http://localhost:3001/api/login",
+        method: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(requestData),
+        success: function (response) {
+            console.log("Login successful:", response); 
+            localStorage.setItem("authToken", response.token);
+            window.location.href = "products.html";
+        },
+    });
+}
+
+function signupUser() {
+    let email = $("#signup-email").val();
+    let password = $("#signup-password").val();
+
+    const requestData = { email, password };
+    $.ajax({
+        url: "http://localhost:3001/api/signup",
+        method: "POST",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify(requestData),
+        success: function () {
+            window.location.href = "login.html";
+        },
+    });
+}
+
+function logoutUser() {
+    localStorage.removeItem("authToken");
+    window.location.href = "login.html";
+}
